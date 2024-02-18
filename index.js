9ba06c87d023f25a8b3e0dcfa9039a915e5d0a09
torikulislamv3 <tamim.officialinfo7@gmail.com>
2024-02-18 16:39:00 +0600

    design done without js

--- index.js
+++ index.js
@@ -1,22 +1,13 @@
 console.log('connected');
 
-<section>
-<div class="left">
-   <div>
-    <div><h3 class="font-raleway font-semibold text-2xl text-[#030712] mb-4">Select Your Seat</h3></div>
-     <div class="flex justify-around">
-        
-    <div class="flex">
-        <img src="./images/seat-gray.png" alt="coming">
-        <p>Available</p>
-    </div>
-    <div class="flex">
-        <img class="h-7" src="./images/seat-green-filled.png" alt="coming">
-        <p class="text-green-600">Selected</p>
-       </div></div>
-   </div>
-   
-</div>
-   
-<div class="right"></div>
-</section>
+
+
+                    
+
+                    
+
+                
+
+
+
+                   
