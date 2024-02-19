cacbe93a04896a7fe2d37de4ba0d1e6f2e4f7403
torikulislamv3 <tamim.officialinfo7@gmail.com>
2024-02-19 13:15:16 +0600

    seat button 40% done

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
+const seatBus =document.querySelectorAll(".seat");
+
+for(index = 0; index < seatBus.length; index++){
+    const seats = seatBus[index];
+    // console.log(Element);
+    seats.addEventListener("click", function(){
+        seats.style.backgroundColor = 'green';
+        // console.log("clicked");
+        // get the seat
+
+        const seatId = seats.querySelector("h3").innerText;
+        // console.log(seatId);
+
+        
+        const backendGm = document.getElementById("backend-gm");
+        // console.log(backendGm);
+        const h4 = document.createElement("h4");
+        h4.innerText = seatId;
+
+        const p = document.createElement("p");
+        p.innerText = 'Economy';
+
+        const h5 =document.createElement("h5");
+        h5.innerText = 550;
+
+        backendGm.appendChild(h4);
+        backendGm.appendChild(p);
+        backendGm.appendChild(h5);
+    })
+}
 
 
                     
